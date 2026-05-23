5b11828a099bfd3d6f21632da4facbc52fb2c3e5
Felippe de Souza Cardoso <cardosofelippe565@gmail.com>
2026-05-23 14:52:11 -0300

    Adicionar arquivos de projeto.

---
+++ TodoListApiii/Dataaa/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TodoListApiii.Models;
+using TodoListApiii.Models;
+
+namespace TodoListApiii.Dataaa
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Tarefa> Tarefas { get; set; }
+    }
+}
---
+++ TodoListApiii/Modelss/Tarefa.cs
@@ -0,0 +1,13 @@
+﻿namespace TodoListApiii.Models
+{
+    public class Tarefa
+    {
+        public int Id { get; set; }
+
+        public string Titulo { get; set; } = string.Empty;
+
+        public string Descricao { get; set; } = string.Empty;
+
+        public bool Concluida { get; set; }
+    }
+}
